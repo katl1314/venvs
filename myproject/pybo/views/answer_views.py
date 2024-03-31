ab7cf990cd718de744fa8be7e72e30b8159c80e4
최민혁 <mhchoi1997@inswave.com>
2024-03-31 23:44:47 +0900

    fix: 답변 리스트 추가하는 함수 변경 add -> append

--- myproject/pybo/views/answer_views.py
+++ myproject/pybo/views/answer_views.py
@@ -12,10 +12,9 @@ def create(question_id):
     question = Question.query.get(question_id)
     content = request.form['content']
     create_date = datetime.now()
-
     answer = Answer(content=content, create_date=create_date)
-    # 질문에 대한 답변 추가
-    question.answer_set.add(answer);
+    # 질문에 대한 답변 추가 => 파이썬 리스트 추가 시 append을 이용한다.
+    question.answer_set.append(answer);
     db.session.commit(); # 데이터베이스에 데이터 추가 적용
     print("Answer가 추가되었습니다.")
     return redirect(url_for('question.detail', question_id=question_id))
