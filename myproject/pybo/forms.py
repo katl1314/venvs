fad45c84eb38353544775ef4bba134778d47d0f1
최민혁 <mhchoi1997@inswave.com>
2024-04-06 23:25:39 +0900

    ddddd

--- myproject/pybo/forms.py
+++ myproject/pybo/forms.py
@@ -3,8 +3,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
-# 질문 폼 생성
+# 질문 폼 생성 FlaskForm을 상속받는다.
+# 제목 필드는 길이가 고정인 StringField를 사용
+# 콘텐츠는 글자 수 제한이 없는 TextField를 사용
+# validators을 통해 유효성 검사 기능 추가 가능
+# DataRequired() => 필수 입력 여부
+# Length() => 길이 점검
+# Email() => 이메일 포맷 여부
+
 class QuestionForm(FlaskForm):
     # StringField(label, validators=[wtforms.validators]
-    subject = StringField('제목', validators=[DataRequired()])
-    content = TextAreaField('내용', validators=[DataRequired()])
+    subject = StringField('제목', validators=[DataRequired('제목 항목을 필수입니다.')])
+    content = TextAreaField('내용', validators=[DataRequired('콘텐츠는 필수입니다.')])
--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -1,11 +1,14 @@
 # 3.31 질문 조회 및 질문 상세 내용 조회 기능 구현
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
+from datetime import datetime
 # 데이터 처리할 모델 불러오기
 from pybo.models import Question
 
 from ..forms import QuestionForm
+
+from pybo import db
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -27,10 +30,22 @@ def detail(question_id):
     question = Question.query.get(question_id)
     return render_template('question/question_detail.html', question=question)
 
-# 질문 등록
-@bp.route('/create/')
+# 질문 등록 GET 또는 POST 처리 가능
+
+@bp.route('/create/', methods=['GET','POST'])
 def create():
-    print("create 되었다 맨이야!")
     # rediect에도 url_for를 사용하여 question.views.py의 list함수를 실행한다.
     form = QuestionForm()
-    return render_template('question/question_form.html', form=form)
+    method = request.method;
+
+    if method == 'POST' and form.validate_on_submit():
+        subject = form.data['subject']
+        content = form.data['content']
+
+        question = Question(subject=subject, content=content, create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit() # 데이터베이스에 최종 반영
+        
+        return redirect(url_for('question.list'))
+    else:
+        return render_template('question/question_form.html', form=form)
